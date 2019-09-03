0f6ce2966c87d7ed6dfcbfe86737b871763d956f
Benjamin Fang <benjaminfang.ol@outlook.com>
2019-09-03 09:37:37 +0800

    rewrite phytree_statistic

--- libexec/phytree_statistic.py
+++ libexec/phytree_statistic.py
@@ -1,47 +1,70 @@
 #! /usr/bin/env python3
+
 import argparse
 import ete3
+import sys
 
-def get_args():
-    args=argparse.ArgumentParser(description='calculate max distance from leaf to root.')
-    args.add_argument('tree_file',type=str,help='tree file name.')
-    args=args.parse_args()
-    tree_file=args.tree_file
-    return tree_file
+
+def get_args(args_in):
+    args = argparse.ArgumentParser(prog='phytree_statistic', description='statistic phylogenetice tree.')
+    args.add_argument('tree_file', type=str, help='tree file name.')
+    args.add_argument('-f_out', default=sys.stdout, help='out file name.')
+    if args_in == None:
+        args = args.parse_args()
+    else:
+        args = args.parse_args(args_in)
+    tree_file, f_out = args.tree_file
+    return tree_file, f_out
+
 
 def get_longest_dist(tree):
     el_max=tree.get_farthest_leaf()
     nl_max=tree.get_farthest_leaf(topology_only=True)
     return el_max,nl_max
 
+
 def get_average_leaf_dist(tree):
-    len_all=[]
+    len_all = []
     for leaf in tree:
         len_all.append([leaf.name,leaf.dist])
-    sort_f=lambda x:x[1]
-    len_all.sort(key=sort_f)
-    shortest_leaf=[]
-    min_len=len_all[0][1]
+    sort_f = lambda x:x[1]
+    len_all.sort(key = sort_f)
+    shortest_leaf = []
+    min_len = len_all[0][1]
     for ele in len_all:
-        if ele[1]==min_len:
+        if ele[1] == min_len:
             shortest_leaf.append(ele)
-    longest_leaf=[]
-    max_len=len_all[-1][1]
+    longest_leaf = []
+    max_len = len_all[-1][1]
     for ele in len_all:
-        if ele[1]==max_len:
+        if ele[1] == max_len:
             longest_leaf.append(ele)
-    leaf_ave_len=sum([x[1] for x in len_all])/len(len_all)
+    leaf_ave_len = sum([x[1] for x in len_all])/len(len_all)
     return shortest_leaf,longest_leaf,leaf_ave_len
 
 
-if __name__ == '__main__':
-    tree_file=get_args()
-    tree=ete3.Tree(tree_file)
-    el_max,nl_max=get_longest_dist(tree)
-    shortest_leaf,longest_leaf,leaf_ave_len=get_average_leaf_dist(tree)
-    print('el_max:',el_max[0].name, el_max[1])
-    print('nl_max:',nl_max[0].name, nl_max[1])
-    print('shortest_leaf:',shortest_leaf)
-    print('longest_leaf:',longest_leaf)
-    print('leaf_ave_len:',leaf_ave_len)
+def print_res(dt, f_out):
+    if f_out != sys.stdout:
+        f_out = open(f_out, 'w')
+    print('max_edge_len:', dt[0], file=f_out)
+    print('max_topology_len:', dt[1], file=f_out)
+    print('shortest_leaf:', dt[2], file=f_out)
+    print('longest_leaf:', dt[3], file=f_out)
+    print('leaf_ave_len:', dt[4], file=f_out)
+    f_out.close()
+    return 0
 
+
+def main(name='phytree_statistic', args=None):
+    myname = 'phytree_statistic'
+    if myname == name:
+        tree_file, f_out = get_args(args)
+        tree = ete3.Tree(tree_file)
+        max_edge_len, max_topology_len = get_longest_dist(tree)
+        shortest_leaf, longest_leaf, leaf_ave_len = get_average_leaf_dist(tree)
+        dt = [max_edge_len, max_topology_len, shortest_leaf, longest_leaf, leaf_ave_len]
+        print_res(dt, f_out)
+
+
+if __name__ == '__main__':
+    main()
